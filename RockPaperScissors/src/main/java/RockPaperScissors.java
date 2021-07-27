95bcdbae19c894da2fd0a36807141e9754476f3c
Jobe Powell <powelljobe@gmail.com>
2021-07-27 10:13:12 -0500

    Added comments for clarity

--- RockPaperScissors/src/main/java/RockPaperScissors.java
+++ RockPaperScissors/src/main/java/RockPaperScissors.java
@@ -17,12 +17,12 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         boolean playAgain = true;
         String ans;
-        while (playAgain) {
+        while (playAgain) { //Calls the game method, then checks if it should play again
             RPS();
             while (true && playAgain) {
                 System.out.println("Would you like to play again? Yes or No");
                 ans = scrn.nextLine();
-                if (ans.equals("")) {
+                if (ans.equals("")) { //Error checking for empty string
                     ans = scrn.nextLine();
                 }
                 if (ans.toUpperCase().equals("NO")) {
@@ -37,7 +37,8 @@ public class RockPaperScissors {
         System.out.println("Thanks for playing!");
         System.exit(0);
     }
-
+    
+    //Method that runs the game of Rock Paper Scissors
     public static void RPS() {
         Random rand = new Random();
         int comChoice = 0; //Randomly chosen computer choice
@@ -47,18 +48,20 @@ public class RockPaperScissors {
 
         System.out.println("How many rounds would you like to play?");
         numRounds = scrn.nextInt();
-        if (numRounds > 10 || numRounds < 1) {
+        if (numRounds > 10 || numRounds < 1) { //Makes sure number of rounds is valid
             System.out.println("ERROR: " + numRounds + " is out of range; must be between 1 and 10");
             System.exit(0);
         }
+        //Iterates for each round being played
         for (int i = 0; i < numRounds; i++) {
             
-            do{
+            do{ //Does not continue game until valid choice is made
                 System.out.println("Would you like to pick:\n\t1.Rock\n\t2.Paper\n\t3.Scissors");
                 choice = scrn.nextInt();
             }while(choice < 1 || choice > 3);
                     
             comChoice = rand.nextInt(3)+1;
+            //If the user chose Rock
             if (choice == 1) {
                 if (comChoice == 1) {
                     System.out.println("Tie!");
@@ -70,7 +73,9 @@ public class RockPaperScissors {
                     System.out.println("User Win!");
                     result[1]++;
                 }
-            } else if (choice == 2) {
+            } 
+            //If the user chose Paper
+            else if (choice == 2) { 
                 if (comChoice == 1) {
                     System.out.println("User Win!");
                     result[1]++;
@@ -81,7 +86,9 @@ public class RockPaperScissors {
                     System.out.println("Computer Win!");
                     result[2]++;
                 }
-            } else {
+            }
+            //If the user chose Scissors
+            else {
                 if (comChoice == 1) {
                     System.out.println("Computer Win!");
                     result[2]++;
@@ -94,9 +101,11 @@ public class RockPaperScissors {
                 }
             }
         }
+        //Prints out results
         System.out.println("Number of Ties: " + result[0]);
         System.out.println("Number of User wins: " + result[1]);
         System.out.println("Number of Computer wins: " + result[2]);
+        //Determines winner
         if (result[1] > result[2]) {
             System.out.println("User wins!");
         } else if (result[1] < result[2]) {
