76cb517d41812c41ea6109b702cf159fa518fe83
Jobe Powell <powelljobe@gmail.com>
2021-07-27 10:09:26 -0500

    Game is functional

--- RockPaperScissors/src/main/java/RockPaperScissors.java
+++ RockPaperScissors/src/main/java/RockPaperScissors.java
@@ -4,24 +4,105 @@
  * and open the template in the editor.
  */
 import java.util.Scanner;
+import java.util.Random;
+
 /**
  *
  * @author powel
  */
 public class RockPaperScissors {
-    public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
+
+    static Scanner scrn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean playAgain = true;
+        String ans;
+        while (playAgain) {
+            RPS();
+            while (true && playAgain) {
+                System.out.println("Would you like to play again? Yes or No");
+                ans = scrn.nextLine();
+                if (ans.equals("")) {
+                    ans = scrn.nextLine();
+                }
+                if (ans.toUpperCase().equals("NO")) {
+                    playAgain = false;
+                } else if (ans.toUpperCase().equals("YES")) {
+                    break;
+                } else {
+                    System.out.println("Invalid input, enter Yes or No");
+                }
+            }
+        }
+        System.out.println("Thanks for playing!");
+        System.exit(0);
+    }
+
+    public static void RPS() {
+        Random rand = new Random();
+        int comChoice = 0; //Randomly chosen computer choice
+        int choice = 0;
         int numRounds = 0;
-        
-        
-        
-        
+        int[] result = new int[3]; //index 0 = numTies, 1 = numWins, 2 = numLoss
+
         System.out.println("How many rounds would you like to play?");
-        numRounds = scn.nextInt();
-        if(numRounds > 10 || numRounds < 1){
+        numRounds = scrn.nextInt();
+        if (numRounds > 10 || numRounds < 1) {
             System.out.println("ERROR: " + numRounds + " is out of range; must be between 1 and 10");
             System.exit(0);
         }
-             
+        for (int i = 0; i < numRounds; i++) {
+            
+            do{
+                System.out.println("Would you like to pick:\n\t1.Rock\n\t2.Paper\n\t3.Scissors");
+                choice = scrn.nextInt();
+            }while(choice < 1 || choice > 3);
+                    
+            comChoice = rand.nextInt(3)+1;
+            if (choice == 1) {
+                if (comChoice == 1) {
+                    System.out.println("Tie!");
+                    result[0]++;
+                } else if (comChoice == 2) {
+                    System.out.println("Computer Win!");
+                    result[2]++;
+                } else {
+                    System.out.println("User Win!");
+                    result[1]++;
+                }
+            } else if (choice == 2) {
+                if (comChoice == 1) {
+                    System.out.println("User Win!");
+                    result[1]++;
+                } else if (comChoice == 2) {
+                    System.out.println("Tie!");
+                    result[0]++;
+                } else {
+                    System.out.println("Computer Win!");
+                    result[2]++;
+                }
+            } else {
+                if (comChoice == 1) {
+                    System.out.println("Computer Win!");
+                    result[2]++;
+                } else if (comChoice == 2) {
+                    System.out.println("User Win!");
+                    result[1]++;
+                } else {
+                    System.out.println("Tie!");
+                    result[0]++;
+                }
+            }
+        }
+        System.out.println("Number of Ties: " + result[0]);
+        System.out.println("Number of User wins: " + result[1]);
+        System.out.println("Number of Computer wins: " + result[2]);
+        if (result[1] > result[2]) {
+            System.out.println("User wins!");
+        } else if (result[1] < result[2]) {
+            System.out.println("Computer wins!");
+        } else {
+            System.out.println("It's a tie!");
+        }
     }
 }
